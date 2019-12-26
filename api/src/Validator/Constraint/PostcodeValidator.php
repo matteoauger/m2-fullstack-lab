a8c276fba4a65155cccd06dfe83ba7fc71308159
Matteo AUGER <augermtt@gmail.com>
2019-12-26 17:46:56 +0100

    Postcode validator

---
+++ api/src/Validator/Constraint/PostcodeValidator.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Validator\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class PostcodeValidator extends ConstraintValidator {   
+    public function validate($value, Constraint $constraint) {
+        
+        // Testing constraint type
+        if (!$constraint instanceof Postcode) {
+            throw new UnexpectedTypeException($constraint, Postcode::class);
+        }
+
+        // In the case of a null or empty value, stopping the function.
+        // A custom validator should ignore null or empty values.
+        // So we let the other constraints (NotBlank, NotNull, etc.) take care of that.
+        if ($value == null || $value == '') {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        if (!preg_match('/^[0-9]{5}$/', $value, $matches)) {
+            // building a constraint violation with the given value 
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
+
+?>
