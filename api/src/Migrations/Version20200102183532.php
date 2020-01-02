2c8b8e34a6287d2104f1ad967e94a0b67b4b9671
Matteo AUGER <augermtt@gmail.com>
2020-01-02 22:03:21 +0100

    Add state migrations

---
+++ api/src/Migrations/Version20200102183532.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200102183532 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE greeting_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE state_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE land_value_claim_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE state (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_A393D2FB5E237E06 ON state (name)');
+        $this->addSql('CREATE TABLE land_value_claim (id INT NOT NULL, dep_code VARCHAR(255) NOT NULL, mutation_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, mutation_type VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, value DOUBLE PRECISION NOT NULL, surface DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE greeting');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE state_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE land_value_claim_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE greeting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE greeting (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE state');
+        $this->addSql('DROP TABLE land_value_claim');
+    }
+}
