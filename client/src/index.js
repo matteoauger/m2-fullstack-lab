99ad401b26cf7c427753e5f84af80e407f585f59
Miguel Dubielowicz <miguel.dubielowicz@etu.univ-lehavre.fr>
2020-01-04 20:08:23 +0100

    add route

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,7 @@ import 'font-awesome/css/font-awesome.css';
 import * as serviceWorker from './serviceWorker';
 // Import your reducers and routes here
 import Welcome from './Welcome';
+import Barchart from './Barchart';
 
 const history = createBrowserHistory();
 const store = createStore(
@@ -32,6 +33,7 @@ ReactDOM.render(
     <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={Welcome} strict={true} exact={true}/>
+        <Route path="/barchart" component={Barchart} strict={true} exact={true}/>
         {/* Add your routes here */}
         <Route render={() => <h1>Not Found</h1>} />
       </Switch>
