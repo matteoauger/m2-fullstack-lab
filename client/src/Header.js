9547f44556bf4e026c268c8b65afe95badbc8b4e
Matteo AUGER <augermtt@gmail.com>
2020-01-07 00:41:40 +0100

    Add Header component

---
+++ client/src/Header.js
@@ -0,0 +1,24 @@
+import React       from 'react';
+import { NavLink } from 'react-router-dom';
+
+/**
+ * Header component.
+ * Represents the header section of the react app.
+ * Contains all links redirecting to the charts.
+ */
+export default class Header extends React.Component {
+    
+    /**
+     * Renders the component as JSX 
+     */
+    render() {
+        return <nav id="header">
+            <NavLink activeClassName="active"       to="/">Imhotep</NavLink>
+            <NavLink activeClassName="active"       to="/timeseries">Evolution des prix</NavLink>
+            <NavLink activeClassName="active"       to="/barchart">Nombre de ventes</NavLink>
+            <NavLink activeClassName="active"       to="/graphs">Répartition géographique</NavLink>
+            <a href={`https://${document.domain}:8443`}>API</a>
+            <a href={`https://${document.domain}:444`}>Admin</a>
+        </nav>
+    }
+}
