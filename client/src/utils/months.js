3fbc16f95dd27c3b94a68074eac5f0086a8193a4
Matteo AUGER <augermtt@gmail.com>
2020-01-07 04:53:50 +0100

    Displaying the full date in bar chart

---
+++ client/src/utils/months.js
@@ -0,0 +1,16 @@
+// see https://www.w3schools.com/jsref/jsref_getmonth.asp
+const month = new Array();
+month[0] = "Janvier";
+month[1] = "Février";
+month[2] = "Mars";
+month[3] = "Avril";
+month[4] = "Mai";
+month[5] = "Juin";
+month[6] = "Juillet";
+month[7] = "Aout";
+month[8] = "Septembre";
+month[9] = "Octobre";
+month[10] = "Novembre";
+month[11] = "Décembre";
+
+export default month;
